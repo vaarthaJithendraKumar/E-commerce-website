4f36aae607ce3e3ab97050323aef1ed95bc4598e
Vaartha Jithendra Kumar <vartajithendrakumar@gmail.com>
2025-08-22 08:41:06 +0530

    Add files via upload

---
+++ male.js
@@ -0,0 +1,24 @@
+let cart = [];
+let total = 0;
+
+// Add product to cart
+function addToCart(name, price) {
+  cart.push({ name, price });
+  total += price;
+  updateCart();
+}
+
+// Update cart UI
+function updateCart() {
+  document.getElementById("cart-count").innerText = cart.length;
+  document.getElementById("cart-total").innerText = total;
+
+  let cartItems = document.getElementById("cart-items");
+  cartItems.innerHTML = "";
+
+  cart.forEach((item) => {
+    let li = document.createElement("li");
+    li.innerText = `${item.name} - $${item.price}`;
+    cartItems.appendChild(li);
+  });
+}
